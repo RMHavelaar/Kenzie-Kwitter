34cacf917c4177f7be135a1503c4a23a6877e6b1
Sarah Beverton <sebryant@gmail.com>
2020-06-04 13:17:35 -0400

    adds route to AddUserScreen

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
-import { HomeScreen, ProfileScreen, NotFoundScreen } from "../../screens";
+import {
+  HomeScreen,
+  ProfileScreen,
+  NotFoundScreen,
+  AddUserScreen,
+} from "../../screens";
 import { ConnectedRoute } from "../connected-route/ConnectedRoute";
 
 export const Navigation = () => (
@@ -18,6 +23,7 @@ export const Navigation = () => (
         path="/profiles/:username"
         component={ProfileScreen}
       />
+      <ConnectedRoute exact path="/register" component={AddUserScreen} />
       <ConnectedRoute path="*" component={NotFoundScreen} />
     </Switch>
   </BrowserRouter>
