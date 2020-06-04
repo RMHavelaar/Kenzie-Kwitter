e542c63b1e116cb0cf197cb426f6fc7a6b19bcad
Sarah Beverton <sebryant@gmail.com>
2020-06-03 20:38:42 -0400

    adds a horizontal rule and Register New User heading for the add user form

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,6 +6,9 @@ export const HomeScreen = () => (
     <MenuContainer />
     <h2>Your favorite microblogging platform</h2>
     <LoginFormContainer />
+    {/* TODO add a route to the registration form instead of having it on home screen */}
+    <hr />
+    <h3>Register New User</h3>
     <AddUserContainer />
   </>
 );
