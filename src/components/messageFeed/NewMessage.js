b451fbe3d343dbbf2483e934bc38cc9e30eeae6d
Robert Havelaar <rmhavelaar@gmail.com>
2020-06-04 21:16:13 -0500

    NewMessage.js setup CCC

--- src/components/messageFeed/NewMessage.js
+++ src/components/messageFeed/NewMessage.js
@@ -1,2 +1,16 @@
-import React, {Component} from "react";
-import Form from "react-bootstrap/Form";
+// import React, {Component} from "react";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button"
+// import "../main.css"
+
+// class  extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {  }
+//     }
+//     render() { 
+//         return (  );
+//     }
+// }
+ 
+// export default ;
