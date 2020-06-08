bb563f38bd63b29fdf4c493a80714ffd9256f6f6
Anie Cross <aniecross@aniecross@ymail.com>
2020-06-07 23:22:21 -0500

    actions addlikes implemented

--- src/redux/actions/likes.js
+++ src/redux/actions/likes.js
@@ -1,20 +1,16 @@
 import api from "../../utils/api";
 
-export const ADD_LIKES = "ADDLIKES";
-export const REMOVE_LIKES = "REMOVELIKES";
-import { ADD_LIKES, REMOVE_LIKES } from "../actions";
+export const ADD_LIKES = "ADD_LIKES"; 
+export const ADD_LIKES_SUCCESS = "ADD_LIKES_SUCCESS";
+export const ADD_LIKES_FAILURE = "ADD_LIKES_FAILURE";
 
-export function userLikes(userLikesCompleted) {
-    return {
-      type: ADD_LIKES, 
-      messageId: userLikesCompleted,
-
-    }
+export const addLikes = (messageId) => async (dispatch, getState) => {
+  const userName = getState().auth.username;
+  try {
+    dispatch({ type: ADD_LIKES});
+    const payload = await api.addLike(messageId);
+    dispatch({ type: ADD_LIKES_SUCCESS, payload});
+  } catch (err){
+    dispatch({ type: ADD_LIKES_FAILURE, err});
   }
-
-export function userRemoveLikes(userLikesRemoved) {
-    return {
-      type: REMOVE_LIKES,
-      messageId: userLikesRemoved,
-    }
-  }  
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./auth";
 import { userReducer } from "./users";
+import { likesReducer } from "./likes";
 
-export default combineReducers({ auth: authReducer, users: userReducer });
+export default combineReducers({ auth: authReducer, users: userReducer, likes: likesReducer });
