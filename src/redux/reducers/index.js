fd36a7434dcf43b4580b640214b93ad0e4cfd507
Sarah Beverton <sebryant@gmail.com>
2020-06-08 15:10:36 -0400

    adds profileReducer to combineReducers()

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./auth";
 import { userReducer } from "./users";
+import { profileReducer } from "./profile";
 
-export default combineReducers({ auth: authReducer, users: userReducer });
+export default combineReducers({ auth: authReducer, users: userReducer, profile: profileReducer });
