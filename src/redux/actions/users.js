7eeb2a3b05fc1e4cce0cc0fee84d9fa5826c3242
Anie Cross <aniecross@aniecross@ymail.com>
2020-06-11 09:52:59 -0500

    updated codes on users.js and messages.js, commented out some items for now.

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -8,10 +8,6 @@ export const GETUSERLIST = "GETUSERLIST";
 export const GETUSERLIST_SUCCESS = "GETUSERLIST_SUCCESS";
 export const GETUSERLIST_FAILURE = "GETUSERLIST_FAILURE";
 
-export const REMOVE_USERLIST = "REMOVE_GETUSERLIST";
-export const REMOVE_USERLIST_SUCCESS = "REMOVE_GETUSERLIST_SUCCESS";
-export const REMOVE_USERLIST_FAILURE = "REMOVE_GETUSERLIST_FAILURE";
-
 /*
  ADDUSER ACTIONS (this is a thunk....)
  THUNKS: --> https://github.com/reduxjs/redux-thunk#whats-a-thunk
@@ -29,24 +25,13 @@ export const addUser = (credentials) => async (dispatch, getState) => {
   }
 };
 
-export const getUserList = (getUserListId) => async (dispatch, getState) => {
-  const userName = getState().auth.username;
+export const getUserList = () => async (dispatch, getState) => {
+  // const userName = getState().auth.username;
   try {
     dispatch({ type: GETUSERLIST});
-    const payload = await api.addMessage(getUserListId);
+    const payload = await api.getUserlist();
     dispatch({ type: GETUSERLIST_SUCCESS, payload});
   } catch (err){
     dispatch({ type: GETUSERLIST_FAILURE, err});
-  }
-}
-
-export const removeGetUserlist = (getUserListId) =>  async (dispatch, getState) => {
-  const userName = getState().auth.username;
-  try {
-    dispatch({ type: REMOVE_GETUSERLIST});
-    const payload = await api.removeMessage(MessageId);
-    dispatch({ type: REMOVE_GETUSERLIST_SUCCESS, payload});
-  } catch (err){
-    dispatch({ type: REMOVE_GETUSERLIST_FAILURE, err});
   }
 }
