bfb9589916500b349327b3676b7015747c70cb3a
Sarah Beverton <sebryant@gmail.com>
2020-06-12 14:16:25 -0400

    adds putPhoto, getPhoto, and deleteUser reducers to exported reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,4 +3,7 @@ import { authReducer } from "./auth";
 import { userReducer } from "./users";
 import { profileReducer } from "./profile";
 import { likesReducer } from "./likes";
-export default combineReducers({ auth: authReducer, users: userReducer, profile: profileReducer, likes: likesReducer });
+import { putPhotoReducer } from "./putPhoto";
+import { getPhotoReducer } from "./getPhoto";
+import { deleteUserReducer } from "./deleteUser";
+export default combineReducers({ auth: authReducer, users: userReducer, profile: profileReducer, likes: likesReducer, putPhoto: putPhotoReducer, getPhoto: getPhotoReducer, deleteUser: deleteUserReducer });
