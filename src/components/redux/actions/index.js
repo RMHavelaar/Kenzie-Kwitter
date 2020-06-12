69f6c9b874618225bf06fc1ed55e39ff3387ba97
Robert Havelaar <rmhavelaar@gmail.com>
2020-06-12 18:48:45 -0500

    wtf happened

--- src/components/redux/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./auth";
-export * from "./users";
-export * from "./profile";
-export * from "./likes";
-export * from "./messages";
---
+++ src/redux/actions/index.js
@@ -0,0 +1,7 @@
+export * from "../../components/loader";
+export * from "../../components/login-form";
+export * from "../../components/menu";
+export * from "../../components/navigation";
+export * from "../../components/add-user";
+export * from "../../components/messages";
+export * from "../../components/profile";
