6d45c92bf2806ae3d7c4699e49e3e9ab837bf2c8
Sarah Beverton <sebryant@gmail.com>
2020-06-12 14:03:10 -0400

    adds deleteUser as a dispatch, maps state of each item (username, displayName) instead of the whole user object

--- src/components/profile/Profile.enhancer.js
+++ src/components/profile/Profile.enhancer.js
@@ -1,18 +1,29 @@
 import { connect } from "react-redux";
 import { profile } from "../../redux/actions/profile";
-
+import { putPhoto } from "../../redux/actions/putPhoto";
+import { login } from "../../redux/actions/auth";
+import { deleteUser } from "../../redux/actions/deleteUser";
 
 // https://react-redux.js.org/using-react-redux/connect-mapstate#connect-extracting-data-with-mapstatetoprops
 const mapStateToProps = (state) => ({
-  username: state.auth.username,
-  user: state.profile.user, 
+  username: state.profile.username,
+  pictureLocation: state.profile.pictureLocation,
+  displayName: state.profile.displayName,
+  about: state.profile.about,
+  googleId: null,
+  createdAt: state.profile.createdAt,
+  updatedAt: state.profile.updatedAt,
   loading: state.profile.loading,
   error: state.profile.error,
+  picture: state.putPhoto.picture,
 });
 
 // https://react-redux.js.org/using-react-redux/connect-mapdispatch#connect-dispatching-actions-with-mapdispatchtoprops
 const mapDispatchToProps = {
   profile,
+  putPhoto,
+  deleteUser,
+  login,
 };
 
 export const enhancer = connect(mapStateToProps, mapDispatchToProps);
