091c949b2bc90c1580c81a403357876541ae30ad
Robert Havelaar <rmhavelaar@gmail.com>
2020-06-13 16:33:33 -0500

    Cleaned up empty files.

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -4,9 +4,27 @@ import Button from "react-bootstrap/Button";
 
 export const Loader = () => {
   return (
-    <Button variant="primary" disabled>
-      <Spinner as="span" animation="grow" role="status" aria-hidden="true" />
-      Loading...
-    </Button>
+<>
+  <Button variant="primary" disabled>
+    <Spinner
+      as="span"
+      animation="border"
+      size="sm"
+      role="status"
+      aria-hidden="true"
+    />
+    <span className="sr-only">Loading...</span>
+  </Button>{' '}
+  <Button variant="primary" disabled>
+    <Spinner
+      as="span"
+      animation="grow"
+      size="sm"
+      role="status"
+      aria-hidden="true"
+    />
+    Loading...
+  </Button>
+</>
   );
 };
