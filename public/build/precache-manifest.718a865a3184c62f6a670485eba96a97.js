54add30be26ea7b7bfacd7b6573bf1e8a1aa174a
Robert Havelaar <rmhavelaar@gmail.com>
2020-07-22 19:58:21 -0500

    delpoyed to firebase

---
+++ public/build/precache-manifest.718a865a3184c62f6a670485eba96a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55ef84477d4673212e7fc117d582bcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "738927b1b07d67844305",
+    "url": "/static/css/main.3673e52b.chunk.css"
+  },
+  {
+    "revision": "29f9cbb6e5541a6bc540",
+    "url": "/static/js/2.648f6ee9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.648f6ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738927b1b07d67844305",
+    "url": "/static/js/main.439c4a2a.chunk.js"
+  },
+  {
+    "revision": "c4164892456c34560c12",
+    "url": "/static/js/runtime-main.62670748.js"
+  },
+  {
+    "revision": "ffc472e02f88b15d12cde2a212bb8895",
+    "url": "/static/media/Kwitter_square.ffc472e0.png"
+  }
+]);
